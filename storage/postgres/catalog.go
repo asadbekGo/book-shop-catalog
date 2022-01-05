7dfbba0a704d7f9b511c6c382c3061f5eb7cd617
Shahboz4131 <shahbozkuchkorov4131@gmail.com>
2022-01-05 11:57:23 +0500

    catalog postgres books

--- storage/postgres/catalog.go
+++ storage/postgres/catalog.go
@@ -1,1 +1,35 @@
 package postgres
+
+import (
+	"database/sql"
+
+	
+	"github.com/jmoiron/sqlx"
+	
+	pb "github.com/asadbekGo/book-shop-catalog/genproto"
+)
+
+type catalogRepo struct {
+	db *sqlx.DB
+}
+
+// NewCatalogRepo ...
+func NewCatalogRepo(db *sqlx.DB) *catalogRepo {
+	return &catalogRepo{db: db}
+}
+
+func (r *catalogRepo) CreateBook(book pb.NewBook) (pb.Book, error) {
+	var id string
+	err := r.db.QueryRow(`
+        INSERT INTO book(book_id, name, author_id, parent_uuid)
+        VALUES ($1,$2,$3,$4) returning book_id`, book.Id, book.Name, book.AuthorId, book.CategoryId.Scan(&id)
+	if err != nil {
+		return pb.Book{}, err
+	}
+	book, err = r.GetBook(id)
+	if err != nil {
+		return pb.Book{}, err
+	}
+
+	return book, nil
+}
